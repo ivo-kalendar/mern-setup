0ec9f1ffcf697382067c8e81071f47520046c523
ivokalendar <ivo.kalendarov@x3mlabs.com>
2024-06-08 00:24:33 +0200

    Separated userRoutes from routes.

--- routes/router.js
+++ routes/router.js
@@ -1,35 +1,7 @@
 import express from 'express'
 const router = express.Router();
+import userRoutes from "./userRoutes.js"
 
-import auth from '../middlewares/auth.js';
-import { 
-    createUser,
-    getAllUsers, 
-    getUser, 
-    getUserName 
-} from '../controllers/userController.js';
-
-router.post('/user', createUser);
-router.get('/user/:id', auth, getUser);
-router.get('/username/:id', auth, getUserName);
-router.get('/allusers', auth, getAllUsers);
-
-
-
-
-// router.get('/ping', (req, res) => {
-//     res.status(401).json({ msg: 'Нема Токен' })
-// })
-
-// router.get('/test', async (req, res) => {
-
-//     const all = await users.find({}).toArray()
-//     res.status(200).json(all)
-// })
-
-// router.post('/login', async (req, res) => {
-//     const user = await users.insertOne(doc)
-//     res.status(200).json({ msg: 'Login Success!', user })
-// })
+router.use("/user", userRoutes)
 
 export default router
---
+++ routes/userRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express'
+const userRoutes = express.Router();
+
+import auth from '../middlewares/auth.js';
+import validateUser from '../middlewares/validateUser.js';
+
+import { 
+    createUser,
+    editUser,
+    getAllUsers, 
+    getUser,
+    removeUser, 
+} from '../controllers/userController.js';
+import addExtraUserData from '../middlewares/addExtraUserData.js';
+
+userRoutes.get('/all', auth, getAllUsers);
+userRoutes.get('/find/:id', auth, getUser);
+
+userRoutes.post('/new', validateUser, addExtraUserData, createUser);
+userRoutes.put('/find/:id', auth, validateUser, editUser);
+
+userRoutes.delete('/find/:id', auth, removeUser);
+
+export default userRoutes
